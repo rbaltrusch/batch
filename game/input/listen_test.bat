1acdd9470c2bd4b56ac038ccbb5223adbfe136cd
rbaltrusch <41642326+rbaltrusch@users.noreply.github.com>
2021-02-06 19:10:17 +0100

    Fix duplicated keys for backward and forward

--- game/input/listen_test.bat
+++ game/input/listen_test.bat
@@ -4,13 +4,13 @@ setlocal enabledelayedexpansion
 call listen
 cls
 set /p input= < input.txt
+set backward=s
 set forward=w
-set forward=s
 set left=a
 set right=d
 
-if !input! == !forward! (echo FORWARD)
-if !input! == !backward! (echo BACKWARD)
+if "!input!" == "!forward!" (echo FORWARD)
+if "!input!" == "!backward!" (echo BACKWARD)
 if !input! == !left! (echo LEFT)
 if !input! == !right! (echo RIGHT)
 goto :loop

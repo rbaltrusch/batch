88a9aeb9fab950cba39da0195137f14d8a7304b4
rbaltrusch <41642326+rbaltrusch@users.noreply.github.com>
2021-02-13 15:14:38 +0100

    Delete extra files

--- training/4_examples/hacker.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-color 0a
-
-:start
-
-echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-
-goto start
